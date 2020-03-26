fe4b4430cef664ab374f64e00a5901d7e826a08e
Will <pn906484@reading.ac.uk>
2020-03-26 12:21:52 +0000

    rm

--- R/functions.R
+++ R/functions.R
@@ -150,7 +150,6 @@ buildingsFileID_Namestr <- function(fIDstr) {
 }
 
 buildingsFileID <- function(fID = NULL) {
-  browser()
   if (!is.null(fID)) return(list("ID" = fID, "fName" = buildingsFileID_Namestr(fID)))
   #making file ID
   library(stringi)
