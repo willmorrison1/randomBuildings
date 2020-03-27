a8f5fb6a83ca07efb94ebaa8a6d11f1fcc3a7ab6
Will <pn906484@reading.ac.uk>
2020-03-27 18:33:54 +0000

    fix

--- R/functions.R
+++ R/functions.R
@@ -101,7 +101,7 @@ createBuildingDistribution <- function(nBuildings,
   #put all in output list
   out <- list()
   out$polygons <- SP_shifted
-  polyDFcentroids <- data.frame(rgeos::gCentroid(buildDistribution$polygons, byid = TRUE))
+  polyDFcentroids <- data.frame(rgeos::gCentroid(SP_shifted, byid = TRUE))
   outDF_DARTcompatible <- outDF_DARTcompatible %>%
     dplyr:mutate(y = y - SPbbox[,"min"]["x"],
                  x = x - min(polyDFcentroids$y))
