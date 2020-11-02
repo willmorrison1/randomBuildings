3be5806df14990e2c8d28193f0364c7f130f5cb8
willmorrison1 <51793934+willmorrison1@users.noreply.github.com>
2020-11-02 20:17:35 +0000

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -169,8 +169,6 @@ buildingsFileID <- function(fID = NULL) {
   if (!is.null(fID)) return(list("ID" = fID, "fName" = buildingsFileID_Namestr(fID)))
   #making file ID
   library(stringi)
-  options(digits.secs = 8)
-  set.seed(print(Sys.time()))
   randomID <- stri_rand_strings(1, 5, pattern = "[A-Z0-9]")
   out <- list()
   out$ID <- randomID
